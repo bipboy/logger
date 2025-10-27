362affba9822bb1108ee1dbd09f9bd92d3f35e22
Dim Kamovski <bipboy@biptik.com>
2025-10-27 19:08:40 +1000

    Enhance package.json to include TypeScript definitions and refactor index.ts to streamline exports from Logger.gen.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
-export * from './res/Logger.res';
-export type {
-  make,
-  info,
-  warn,
-  error,
-  debug,
-  verbose,
-  log,
-  trace
-} from './res/Logger.gen';
+export * from './res/Logger.gen';
 export type {logLevel, consoleLoggerType} from './res/Logger.gen';
